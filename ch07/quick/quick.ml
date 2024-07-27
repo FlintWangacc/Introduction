6662c8533ad6c9e2de8f5d7d51d36876528de8f5
hmsjwzb <hmsjwzb@zoho.com>
2024-07-27 15:33:48 +0800

    Chapter 7: Quicksort
    	Quicksort fast example

---
+++ ch07/quick/quick.ml
@@ -0,0 +1,17 @@
+let partition arr =
+  let x = List.rev arr |> List.hd in
+  let nl_less = List.filter (fun t -> t < x) arr in
+  let nl_larger = List.filter (fun t -> t > x) arr in
+  nl_less, x, nl_larger
+
+let rec quicksort arr =
+  (*let _ = List.iter (Printf.printf "%d\t") arr; Printf.printf "\n" in*)
+  if List.length arr > 1 then
+    let (a1, q, a2) = partition arr in
+    let na1 = quicksort a1 in
+    let na2 = quicksort a2 in
+    na1 @ (q::na2)
+  else
+    arr
+
+let _ = quicksort [2; 8; 7; 1; 3; 5; 6; 4] |> List.iter (Printf.printf "%d\t") 
