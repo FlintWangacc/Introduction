688648665d2db8dddde694183616aa508e82815b
hmsjwzb <hmsjwzb@zoho.com>
2025-05-27 08:38:35 +0800

    Elements of dynamic programming
    	lookup_chain
    	recursive_matrix_chain

--- ch14/matrixChainMultiplication/matrixChain.ml
+++ ch14/matrixChainMultiplication/matrixChain.ml
@@ -74,6 +74,31 @@ let matrix_chain_order p n =
   List.iter idx_lst
       ~f:(fun l -> compute_chain l); m, s
 
+
+
+let rec recursive_matrix_chain p i j m =
+  let n = j - i in
+  if n = 0 then 0
+  else
+  (*let _ = Caml.Printf.printf "i:%d, j:%d\n" i j in
+  let _ = (!m).(i).(j) <- Int.max_value in*)
+  let iter_lst = List.init n ~f:(fun idx -> idx + i) in
+  let q_list = List.map iter_lst
+                  ~f:(fun k ->
+                        recursive_matrix_chain p i k m +
+                        recursive_matrix_chain p (k+1) j m +
+                        p.(i-1) * p.(k) * p.(j)
+                      ) in
+ (*let _ = Caml.Printf.printf "q_list:"; print_int_list q_list; Caml.Printf.printf "\n" in*)
+  let q = List.fold_left q_list ~init:Int.max_value ~f:(fun acc t -> if acc < t then acc else t) in
+  (!m).(i).(j) <- q;q
+
+let recursive_matrix_chain_entry p i j =
+  let n = j in
+  let m = ref (Array.make_matrix ~dimx:(n + 2) ~dimy:(n + 2) 0) in
+  recursive_matrix_chain p i j m
+
+
 let p = [| 30; 35; 15; 5; 10; 20; 25|]
 
 let rec print_optimal_parens s i j =
@@ -84,10 +109,34 @@ let rec print_optimal_parens s i j =
     let second = print_optimal_parens s (s.(i).(j) + 1) j in
     Printf.sprintf "(%s%s)" first second
 
+let rec lookup_chain m p i j =
+  if m.(i).(j) < Int.max_value then
+    m.(i).(j)
+  else
+    if i = j then
+      (m.(i).(j) <- 0;0)
+    else
+      let iter_lst = List.init (j - i - 1) ~f:(fun idx -> idx + i) in
+      let q_list = List.map iter_lst
+                  ~f:(fun k ->
+                      lookup_chain m p i k +
+                      lookup_chain m p (k + 1) j + p.(i-1) * p.(k) * p.(j)) in
+      let q = List.fold_left q_list ~init:Int.max_value ~f:(fun acc t -> if acc < t then acc else t) in
+      let _ = if q < m.(i).(j) then m.(i).(j) <- q else () in
+      m.(i).(j)
+
+let memoized_matrix_chain p n =
+  let m = Array.make_matrix ~dimx:(n+1) ~dimy:(n+1) Int.max_value in
+  lookup_chain m p 1 n
+
 let () =
   let m, s = matrix_chain_order p 6 in
   let par_string = print_optimal_parens s 1 6 in
   (*print_int_array_array "m" m;
-  print_int_array_array "s" s*)
-  Caml.Printf.printf "%s\n" par_string
+  print_int_array_array "s" s;*)
+  Caml.Printf.printf "%s\n" par_string;
+  let rmc = recursive_matrix_chain_entry p 2 4 in
+  Caml.Printf.printf "%d\n" rmc;
+  Caml.Printf.printf "%d\n" m.(2).(4);
+  Caml.Printf.printf "%d\n" (lookup_chain m p 2 4)
 
