a2c142703b271ad209d0221fad0ee21b17f94752
hmsjwzb <hmsjwzb@zoho.com>
2025-12-28 18:40:21 +0800

    [DFS] Toplogical sort

--- ch20/graph.ml
+++ ch20/graph.ml
@@ -1,5 +1,7 @@
+open Stdlib
+
 type color = WHITE | GRAY | BLACK
-type vertex_info = { color : color ref; d : int ref ; p : vertex_info option ref; idx : int}
+type vertex_info = { color : color ref; d : int ref ; f : int ref; p : vertex_info option ref; idx : int}
 
 class graph (filename : string) =
     object (self)
@@ -8,15 +10,21 @@ class graph (filename : string) =
         val mutable num_vertices = 0
         val mutable is_directed = false
         val mutable vertexes : vertex_info array = [||]
+        val mutable top_order : int list = []
 
         initializer
             self#read_file2 filename;
             vertexes <- Array.init
                             (num_vertices + 1)
-                            (fun t -> { color = ref WHITE; d = ref max_int; p = ref None; idx = t})
+                            (fun t -> { color = ref WHITE; d = ref max_int; f = ref max_int; p = ref None; idx = t})
 
         method private vertexes_to_int vv =
-            Array.find_index (fun t -> t = vv) vertexes
+            match (Array.find_index (fun t -> t = vv) vertexes) with
+            | Some  idx -> idx
+            | None -> failwith "Not found"
+        
+        method toplogical_order () : int list =
+            top_order
 
         method private read_file2 (filename : string) =
             let channel = open_in filename in
@@ -33,11 +41,11 @@ class graph (filename : string) =
                             adjacency_list.(v) <- u :: adjacency_list.(v) in
                     let n, directed = handle_first hd in
                     let handle_line line =
-                        let _ = print_string line in
+                        let _ = print_string line;print_newline () in
                         match String.split_on_char ' ' line |> List.map int_of_string with
                         | [u; v] -> add_edge u v directed
                         | _ -> failwith "Illegal line content" in
-                    let _ = adjacency_list <- Array.make (n + 1) []; num_vertices <- n; is_directed <- directed in
+                    let _ = adjacency_list <- Stdlib.Array.make (n + 1) []; num_vertices <- n; is_directed <- directed in
                     List.iter (fun t -> handle_line t) tl
                 )
             | _ -> failwith "illegal file content"
@@ -77,20 +85,20 @@ class graph (filename : string) =
         method is_directed = is_directed
 
         method print_graph =
-            Printf.printf "Graph (%d vertices, %s):\n" num_vertices
+            Stdlib.Printf.printf "Graph (%d vertices, %s):\n" num_vertices
                 (if is_directed then "directed" else "undirected");
             for i = 0 to num_vertices - 1 do
-                Printf.printf "Vertex %d: " (i + 1);
-                List.iter (fun x -> Printf.printf "%d " x) adjacency_list.(i);
+                Stdlib.Printf.printf "Vertex %d: " (i + 1);
+                List.iter (fun x -> Stdlib.Printf.printf "%d " x) adjacency_list.(i + 1);
                 print_endline ""
             done
 
 
         method bfs2 (s : int) =
-            vertexes.(s) <- {color = ref GRAY; d = ref 0; p = ref None; idx = s};
+            vertexes.(s) <- {color = ref GRAY; d = ref 0; f = ref 0; p = ref None; idx = s};
             let rec bfs q =
-                let q' = Queue.create () in
-                match Queue.take_opt q with
+                let q' = Stdlib.Queue.create () in
+                match Stdlib.Queue.take_opt q with
                 | None -> ()
                 | Some  e ->
                     (*let _ = Printf.printf "%d\n" e.idx in*)
@@ -102,18 +110,18 @@ class graph (filename : string) =
                             v.color := GRAY;
                             v.d := !(u.d) + 1;
                             v.p := Some u;
-                            Queue.add v q'
+                            Stdlib.Queue.add v q'
                         )
                         | _ -> () in
                     let adj_lst = adjacency_list.(e.idx) |> List.map (fun i -> vertexes.(i)) in
                     List.iter (fun t -> handle_v e t) adj_lst; e.color := BLACK; bfs q'
                 ) in
-                let q = Queue.create () in  let _ = Queue.add vertexes.(s) q in
+                let q = Stdlib.Queue.create () in  let _ = Stdlib.Queue.add vertexes.(s) q in
                 bfs q 
 
         method bfs (s : int)=
-            vertexes.(s) <- {color = ref GRAY; d = ref 0; p = ref None; idx = s};
-            let bfs_queue = Queue.create () in
+            vertexes.(s) <- {color = ref GRAY; d = ref 0; f = ref 0; p = ref None; idx = s};
+            let bfs_queue = Stdlib.Queue.create () in
             let handle_v u = function
             | {color = color; d = d; p = p} as v ->
                 match !color with
@@ -121,16 +129,44 @@ class graph (filename : string) =
                     v.color := GRAY;
                     v.d := !(u.d) + 1;
                     v.p := Some u;
-                    Queue.add v bfs_queue
+                    Stdlib.Queue.add v bfs_queue
                 )
             | _ -> () in
-            let _ = Queue.add vertexes.(s) bfs_queue in
-            while not (Queue.is_empty bfs_queue) do
-                let u = Queue.take bfs_queue in
+            let _ = Stdlib.Queue.add vertexes.(s) bfs_queue in
+            while not (Stdlib.Queue.is_empty bfs_queue) do
+                let u = Stdlib.Queue.take bfs_queue in
                 let adj_lst = adjacency_list.(u.idx) |> List.map (fun i -> vertexes.(i)) in
                 List.iter (fun t -> handle_v u t) adj_lst;u.color := BLACK
             done;
             ()
+
+        method dfs () =
+            let time = ref 0 in
+            let rec dfs_visit u time =
+                time := !time + 1;
+                u.d := !time;
+                u.color := GRAY;
+                let handle_v v u  =
+                    if !(v.color) == WHITE then
+                        (v.p := Some u;
+                        dfs_visit  v time)
+                    else () in
+                
+                match self#vertexes_to_int u with
+                | u_idx ->
+                    let adj_lst = adjacency_list.(u_idx) |> List.map (fun i -> vertexes.(i)) in 
+                    List.iter (fun v -> handle_v v u ) adj_lst; Printf.printf "u_idx:%d\n" u_idx;
+                time := !time + 1;
+                u.f := !time;
+                let _ = Printf.printf "insert %d front\n" (self#vertexes_to_int u) in
+                top_order <- self#vertexes_to_int u :: top_order;
+                u.color := BLACK
+            in
+                Stdlib.Array.iteri (fun idx v ->
+                    if idx > 0 then
+                        if !(v.color) = WHITE then dfs_visit v time
+                    else ()
+                ) vertexes
 
         method print_path (s : int) (v : int) =
             let _ = Printf.printf "s:%d, v:%d\n" s v in
@@ -140,10 +176,10 @@ class graph (filename : string) =
                 let pv = vertexes.(v).p in
                 match !(pv) with
                 | None -> failwith "no path"
-                | Some p -> let vpv = self#vertexes_to_int p |> Option.get in (self#print_path s vpv) @  [v]
+                | Some p -> let vpv = self#vertexes_to_int p in (self#print_path s vpv) @  [v]
 
 
-        method private generate_dot_content (add_line : string -> unit) =
+        (*method private generate_dot_content (add_line : string -> unit) =
             let graph_type = if is_directed then "digraph" else "graph" in
             let edge_op = if is_directed then "->" else "--" in
             
@@ -169,6 +205,38 @@ class graph (filename : string) =
                 ) neighbors
             done;
             
+            add_line "}"*)
+        method private generate_dot_content (add_line : string -> unit) =
+            let graph_type = if is_directed then "digraph" else "graph" in
+            add_line (Printf.sprintf "%s G {" graph_type);
+            add_line "    layout=dot;";
+            add_line "    rankdir=LR;";
+            add_line "    node [shape=circle];";
+    
+            (* Add all vertices *)
+            for i = 1 to num_vertices do
+                add_line (Printf.sprintf "    %d;" i)
+            done;
+    
+            (* Add edges - CRITICAL FIX: Different handling for directed vs undirected *)
+            if is_directed then
+                (* For directed graphs: all edges get arrows *)
+                for i = 1 to num_vertices do
+                    let u = i in
+                    List.iter (fun v ->
+                        add_line (Printf.sprintf "    %d -> %d;" u v)
+                    ) adjacency_list.(i)
+                done
+            else
+                (* For undirected graphs: only add edges where v >= u to avoid duplicates *)
+                for i = 1 to num_vertices do
+                    let u = i in
+                    let neighbors = List.filter (fun v -> v >= u) adjacency_list.(i) in
+                    List.iter (fun v ->
+                        add_line (Printf.sprintf "    %d -- %d;" u v)
+                    ) neighbors
+            done;
+    
             add_line "}"
 
         (* Generate DOT format representation of the graph *)
@@ -203,13 +271,16 @@ class graph (filename : string) =
     end
 
 let () =
-    let g = new graph "graph.txt" in
+    let g = new graph "/mnt/c/Users/hmsjw/Introduction/ch20/deepseek.txt" in
     g#print_graph;
-    g#bfs 1;
-    List.iter (fun t -> Printf.printf "%d " t) (g#print_path 1 3); print_newline ();
+    let _ = g#dfs () in
+    List.iter (fun t -> Printf.printf "%d " t) (g#print_path 1 7); print_newline ();
     let dot_file = g#to_dot () in
     Printf.printf "DOT file generated: %s\n" dot_file;
     
     (* Generate picture (requires Graphviz) *)
     let picture_file = g#dump_picture () in
-    Printf.printf "Picture generated: %s\n" picture_file
+    Printf.printf "Picture generated: %s\n" picture_file;
+    let top = g#toplogical_order ()  in
+    List.iter (fun t -> Printf.printf "%d " t) top;
+    print_newline ()
